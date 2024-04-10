764c45718ff8659717d1de5e4cfa065af5b510ca
francis-mwas <mwangif184@gmail.com>
2024-04-10 23:09:49 +0300

    added navigation header

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
-  return (
-        <div>
-          <h2>Hello world</h2>
-        </div>
-  );
+  return <div></div>;
 }
