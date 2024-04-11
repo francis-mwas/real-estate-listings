541fb6f9c40da4fb3100d6997c51ab49764d7fe0
francis-mwas <mwangif184@gmail.com>
2024-04-11 22:38:21 +0300

    added clerk authentication

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './_components/Header';
 
+//To allow children rendering from the main layout page
 function Provider({ children }) {
   return (
     <div>
       <Header />
-      {children}
+      <div className="mt-32">{children}</div>
     </div>
   );
 }
--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -5,19 +5,25 @@ import { Button } from '@/components/ui/button';
 import { Plus } from 'lucide-react';
 import Link from 'next/link';
 import { useParams, usePathname } from 'next/navigation';
+import { UserButton, useUser } from '@clerk/nextjs';
 
 function Header() {
   const path = usePathname();
   useEffect(() => {
     console.log(path);
   }, []);
+  const { user, isSignedIn } = useUser();
   return (
     <div className="p-6 px-10 flex justify-between shadow-md fixed top-0 w-full z-10 bg-white">
       <div className="flex gap-12 items-center">
         <Image src={'/logo.svg'} width={150} height={150} alt="logo" />
         <ul className="hidden md:flex gap-10">
           <Link href={'/'}>
-            <li className="hover:text-primary font-medium text-sm cursor-pointer">
+            <li
+              className={`'hover:text-primary font-medium text-sm cursor-pointer' ${
+                path == '/' && 'text-primary'
+              }`}
+            >
               For Sale
             </li>
           </Link>
@@ -29,12 +35,12 @@ function Header() {
           </li>
         </ul>
       </div>
-      <div className="flex gap-2">
+      <div className="flex gap-2 items-center">
         <Button className="flex gap-2">
           {' '}
           <Plus className="h-5 w-5" /> Post Your Ad
         </Button>
-        <Button variant="outline">Login</Button>
+        {isSignedIn ? <UserButton /> : <Button variant="outline">Login</Button>}
       </div>
     </div>
   );
